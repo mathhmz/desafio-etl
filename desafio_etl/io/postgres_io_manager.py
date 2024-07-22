e36ac68398a974bb19abe5503e77218d51161de4
mathhmz <rm551843@fiap.com.br>
2024-07-22 07:16:09 +0000

    refactor: Documentando e separando as funcoes do I/o Manager.

--- desafio_etl/io/postgres_io_manager.py
+++ desafio_etl/io/postgres_io_manager.py
@@ -1,78 +1,69 @@
-from dagster import (
-    IOManager,
-    io_manager,
-    InitResourceContext,
-    InputContext,
-    OutputContext,
-    StringSource
-)
+from dagster import IOManager, io_manager, InitResourceContext, InputContext, OutputContext, StringSource
 import pandas as pd
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
+from ..database.base import create_and_update_table
 
+
+
+# Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
-Base = declarative_base()
 
-class Proposicao(Base):
-    __tablename__ = 'proposicoes_silver'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    author = Column(String)
-    presentationDate = Column(DateTime)
-    ementa = Column(String)
-    regime = Column(String)
-    situation = Column(String)
-    propositionType = Column(String)
-    number = Column(String)
-    year = Column(Integer)
-    city = Column(String, default="Belo Horizonte")
-    state = Column(String, default="Minas Gerais")
-
-class Tramitacao(Base):
-    __tablename__ = 'tramitacoes_silver'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    createdAt = Column(DateTime)
-    description = Column(String)
-    local = Column(String)
 
 class PostgresPandasIOManager(IOManager):
+    """
+    Classe para gerenciar I/O no Dagster usando PostgreSQL e Pandas.
+
+    Métodos:
+        __init__(user, password, host, database, port): Inicializa a conexão com o banco de dados.
+        handle_output(context, obj): Salva um DataFrame em uma tabela PostgreSQL.
+        load_input(context): Carrega dados de uma tabela PostgreSQL em um DataFrame.
+    """
     def __init__(self, user: str, password: str, host: str, database: str, port: str) -> None:
+        """
+        Inicializa a conexão com o banco de dados.
+
+        Args:
+            user (str): Nome de usuário do banco de dados.
+            password (str): Senha do banco de dados.
+            host (str): Endereço do host do banco de dados.
+            database (str): Nome do banco de dados.
+            port (str): Porta de conexão do banco de dados.
+        """
         self.user = user
         self.password = password
-        self.database = database
         self.host = host
+        self.database = database
         self.port = port
         self.engine = create_engine(f'postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}')
 
     def handle_output(self, context: OutputContext, obj):
+        """
+        Salva um DataFrame em uma tabela PostgreSQL.
+
+        Args:
+            context (OutputContext): Contexto de saída do Dagster.
+            obj (DataFrame): Objeto DataFrame a ser salvo.
+        """
         if obj is None:
             return
 
         table_name = context.asset_key.to_python_identifier()
-
-        if table_name == 'proposicoes_silver':
-            Base.metadata.tables['proposicoes_silver'].create(self.engine, checkfirst=True)
-        elif table_name == 'tramitacoes_silver':
-            Base.metadata.tables['tramitacoes_silver'].create(self.engine, checkfirst=True)
-
-        obj.to_sql(table_name, self.engine, if_exists='replace', index=False)
-        with self.engine.connect() as conn:
-            if table_name == 'proposicoes_silver':
-                conn.execute(text("ALTER TABLE proposicoes_silver  ADD COLUMN id BIGSERIAL PRIMARY KEY;"))
-                conn.commit()
-            elif table_name == 'tramitacoes_silver':
-                conn.execute(text("ALTER TABLE tramitacoes_silver ADD COLUMN propositionID bigint;"))
-                conn.commit()
-                conn.execute(text("UPDATE tramitacoes_silver ts SET propositionID = ps.id FROM proposicoes_silver ps WHERE ts.number = ps.number;"))
-                conn.commit()
-                conn.execute(text("ALTER TABLE tramitacoes_silver ADD CONSTRAINT fk_propositionID FOREIGN KEY (propositionID) REFERENCES proposicoes_silver(id)"))
-                conn.commit()
+        create_and_update_table(self.engine, table_name, obj)
         context.add_output_metadata({"db": self.database, "table_name": table_name})
 
     def load_input(self, context: InputContext):
+        """
+        Carrega dados de uma tabela PostgreSQL em um DataFrame.
+
+        Args:
+            context (InputContext): Contexto de entrada do Dagster.
+
+        Returns:
+            DataFrame: DataFrame contendo os dados carregados da tabela.
+        """
         table_name = context.upstream_output.asset_key.to_python_identifier()
         df = pd.read_sql(f"SELECT * FROM public.{table_name}", self.engine)
         return df
@@ -87,6 +78,15 @@ class PostgresPandasIOManager(IOManager):
     }
 )
 def postgres_pandas_io_manager(init_context: InitResourceContext) -> PostgresPandasIOManager:
+    """
+    Configuração do IO Manager no Dagster.
+
+    Args:
+        init_context (InitResourceContext): Contexto de inicialização do recurso no Dagster.
+
+    Returns:
+        PostgresPandasIOManager: Instância do gerenciador de I/O configurado.
+    """
     return PostgresPandasIOManager(
         user=os.getenv('SILVER_DB_USER'),
         password=os.getenv('SILVER_DB_PASSWORD'),
